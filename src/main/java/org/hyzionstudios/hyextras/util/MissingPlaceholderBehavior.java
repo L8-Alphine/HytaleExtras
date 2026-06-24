78e37418bd575c49582711c19dc52107b69a7724
Krystian Carter <krystian.carter18@gmail.com>
2026-06-24 13:48:56 -0400

    Added Placeholder Fallback.

---
+++ src/main/java/org/hyzionstudios/hyextras/util/MissingPlaceholderBehavior.java
@@ -0,0 +1,8 @@
+package org.hyzionstudios.hyextras.util;
+
+/** Controls how unresolved optional placeholders are rendered. */
+public enum MissingPlaceholderBehavior {
+    KEEP_ORIGINAL,
+    EMPTY,
+    ERROR
+}
